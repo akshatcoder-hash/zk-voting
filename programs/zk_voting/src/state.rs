c5b3d962e50badecbef7eb9efd5a962e32f55421
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-12 16:14:00 +0530

    state with zk

--- programs/zk_voting/src/state.rs
+++ programs/zk_voting/src/state.rs
@@ -1,17 +1,19 @@
 use anchor_lang::prelude::*;
+use crate::zk::elgamal::ElGamalCiphertext;
 
 #[account]
 pub struct DaoState {
     pub authority: Pubkey,
     pub proposal_count: u64,
+    pub public_key: u64,
+    pub private_key: u64,
 }
 
 #[account]
 pub struct Proposal {
     pub id: u64,
     pub description: String,
-    pub yes_votes: u64,
-    pub no_votes: u64,
+    pub encrypted_votes: Vec<ElGamalCiphertext>,
     pub is_active: bool,
 }
 
