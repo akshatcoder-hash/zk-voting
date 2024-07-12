c597639e3edcc1b2d1225cea2b60450747b6c0fa
Akshat Sharma <akshatsharma0023@outlook.com>
2024-07-12 16:07:36 +0530

    putting instructions together

---
+++ programs/zk_voting/src/instructions/mod.rs
@@ -0,0 +1,11 @@
+pub mod initialize;
+pub mod create_proposal;
+pub mod cast_vote;
+pub mod get_results;
+pub mod reward_participant;
+
+pub use initialize::*;
+pub use create_proposal::*;
+pub use cast_vote::*;
+pub use get_results::*;
+pub use reward_participant::*;
